5fe4739cdcc80dc0f3752da922094af5287816ee
cedwards <cedwards@hugeinc.com>
2016-02-15 12:36:55 -0500

    adding a method for loading mocks to the feature tests

--- tests/FeatureModel_tests.py
+++ tests/FeatureModel_tests.py
@@ -4,27 +4,48 @@ from mock import mock_loader
 from hipchat import browser_map
 
 
-def test_stat_parse():
-    mock = mock_loader.load_mock('features/transforms2d')
-    feature = FeatureModel("transforms2d")
+def get_feature_model(slug):
+    mock = mock_loader.load_mock('features/%s' % slug)
+    feature = FeatureModel(slug)
     feature.parse(mock)
+    return feature
+
+
+def test_stat_parse():
+    feature = get_feature_model('transforms2d')
     assert_equals(feature.support.get('safari').get('y'), '9')
     assert_equals(feature.support.get('opera').get('n'), '9')
 
 
 def test_get_min_support_by_flags():
-    mock = mock_loader.load_mock('features/flexbox')
-    feature = FeatureModel('flexbox')
-    feature.parse(mock)
+    feature = get_feature_model('flexbox')
     flags = ['y x', 'a', 'a x']
+
+    version, notes = feature.get_min_support_by_flags('opera', flags)
+    assert_equals(version, '15')
+    assert_equals(len(notes), 0)
+
     version, notes = feature.get_min_support_by_flags('ie', flags)
-    assert_equals(version, '10')
+    assert_equals(version, '11')
+    assert_equals(len(notes), 1)
+
+
+def test_get_version_notes_from_flag():
+    feature = get_feature_model('flexbox')
+    flag, notes = feature.get_version_notes_from_flag('a x #4 #3')
+    assert_equals(flag, 'a x')
+    assert_equals(len(notes), 2)
+    flag, notes = feature.get_version_notes_from_flag('a #4')
+    assert_equals(flag, 'a')
+    assert_equals(len(notes), 1)
+    flag, notes = feature.get_version_notes_from_flag('y x')
+    assert_equals(flag, 'y x')
+    assert_equals(notes, None)
 
 
 def test_get_relevant_notes():
-    mock = mock_loader.load_mock('features/flexbox')
-    feature = FeatureModel('flexbox')
-    feature.parse(mock)
+    # todo test websockets
+    feature = get_feature_model('flexbox')
     flags = ['y x', 'a', 'a x']
     keys = browser_map.keys()
     notes = feature.get_relevant_notes(keys, flags)
