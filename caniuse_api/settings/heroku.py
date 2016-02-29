34f2d6ba02e106cdf1a2e9e0270306bd9c38d735
cedwards <cedwards@hugeinc.com>
2016-02-29 09:57:01 -0500

    adding secret key and google auth info to heroku settings mapping

--- caniuse_api/settings/heroku.py
+++ caniuse_api/settings/heroku.py
@@ -1,4 +1,8 @@
 import os
 from .base import *
 
+DEBUG = False
 TOKEN = os.environ.get('TOKEN', None)
+SECRET_KEY = os.environ.get('GOOGLE_CLIENT_ID', None)
+GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID', None)
+GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET', None)
