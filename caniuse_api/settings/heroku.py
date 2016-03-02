6c25cac6f1c13edc6c8a7eaaeeb723042fc679f4
cedwards <cedwards@hugeinc.com>
2016-03-02 10:07:48 -0500

    heroku config was pointed at wrong envar for secret you big dummy

--- caniuse_api/settings/heroku.py
+++ caniuse_api/settings/heroku.py
@@ -3,6 +3,6 @@ from .base import *
 
 DEBUG = False
 TOKEN = os.environ.get('TOKEN', None)
-SECRET_KEY = os.environ.get('GOOGLE_CLIENT_ID', None)
+SECRET_KEY = os.environ.get('SECRET_KEY', None)
 GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID', None)
 GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET', None)
